5a6f2d6e40fd438c2c0a070f3206c18bd6d380cc
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2021-01-08 21:20:09 +0100

    Add files via upload

---
+++ pages/add_to_cart.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+
+class CartAdding(BasePage):
