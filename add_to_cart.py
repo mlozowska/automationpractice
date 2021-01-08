ad667e3589b44dd414c2661d34361ddc5dc82b53
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2021-01-08 21:19:37 +0100

    Delete add_to_cart.py

--- add_to_cart.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from pages.base_page import BasePage
-
-class CartAdding(BasePage):
